2cda7d190956d259bfa31a38e830ae44c9ac3352
Chan Jeong <chan.jeong@lge.com>
2024-11-19 06:39:48 +0000

    gnuplot: make it possible to run outside of script dir
    The gnuplot script can be invoked anywhere outside of its dir:
      $ ./script/gnuplot/gnuplot.sh node.gpdata node.png

--- scripts/gnuplot/gnuplot.sh
+++ scripts/gnuplot/gnuplot.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-
 if [ $# -ne 2 ]; then
 	echo "Usage: $0 input_file output_file"
 	exit 1
 fi
 
+GP_SCRIPT_DIR=$(dirname $0)
 GP_INPUT_FILE=$1
 GP_OUTPUT_FILE=$2
 
+export GP_SCRIPT_DIR
 export GP_INPUT_FILE
 
-exec gnuplot ttsched.gpi > $GP_OUTPUT_FILE
+exec gnuplot $GP_SCRIPT_DIR/ttsched.gpi > $GP_OUTPUT_FILE
